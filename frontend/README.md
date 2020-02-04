38442bdaef0205c1b1ac42f8d14bc405644b28c6
golkhandani <golkhandani@outlook.com>
2020-02-04 13:11:08 +0330

    init angular

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# MyFirstProject
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.24.
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
